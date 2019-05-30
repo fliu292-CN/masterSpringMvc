a8350fc0e38a70da5cb8e12ab634268b7186e4fe
fliu292-CN <fliu292@163.com>
2019-05-30 16:39:58 +0800

    Rename Package Name

--- src/main/java/masterspringmvc/MasterspringmvcApplication.java
+++ src/main/java/masterspringmvc/MasterspringmvcApplication.java
@@ -1,4 +1,4 @@
-package masterspringmvc.masterspringmvc;
+package masterspringmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/masterspringmvc/MasterspringmvcApplicationTests.java
+++ src/test/java/masterspringmvc/MasterspringmvcApplicationTests.java
@@ -1,4 +1,4 @@
-package masterspringmvc.masterspringmvc;
+package masterspringmvc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
